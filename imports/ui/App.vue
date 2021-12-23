bc53a34272b7189c000c5a72b88610aec4d1f7a6
miazyn.anton <miazyn.anton@gmail.com>
2021-12-23 12:17:32 +0200

    Meteor.js project initialization

---
+++ imports/ui/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <info />
+  </div>
+</template>
+
+<script>
+import Info from './components/Info.vue'
+
+export default {
+  components: {
+    Info
+  }
+}
+</script>
+
+<style>
+  body {
+    font-family: sans-serif;
+    padding: 10px;
+  }
+</style>
