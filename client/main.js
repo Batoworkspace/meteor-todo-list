0dd4324e673195214dd2b20140d29af02abb3499
miazyn.anton <miazyn.anton@gmail.com>
2021-12-23 12:34:30 +0200

    Vuetify connected

--- client/main.js
+++ client/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 
 import '../imports/ui/plugins'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
 import App from '../imports/ui/App.vue'
 
 Meteor.startup(() => {
+  const vuetify = new Vuetify()
+
   new Vue({
     el: '#app',
-    ...App
+    ...App,
+    vuetify
   })
 })
